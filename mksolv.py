d92d56ac9897c10b4416d1a85db4b60e67b87c54
SahayanKY <a1c4f0x324@gmail.com>
2021-06-16 20:09:59 +0900

    feat: 今後の実装の計画中

--- mksolv.py
+++ mksolv.py
@@ -2,6 +2,7 @@ import argparse
 import platform
 import sys
 
+import numpy as np
 from rdkit import rdBase
 from rdkit import Chem
 from rdkit.Chem import AllChem
@@ -10,9 +11,9 @@ def generateConformer(format, value, needAddHs):
     if format == 'SMILES':
         mol = Chem.MolFromSmiles(value)
     elif format == 'MOL':
-        mol = Chem.MolFromMolFile(value)
+        mol = Chem.MolFromMolFile(value,removeHs=False)
     elif format == 'MOL2':
-        mol = Chem.MolFromMol2File(value)
+        mol = Chem.MolFromMol2File(value,removeHs=False)
     else:
         raise ValueError("invalid choice: {} (choose from 'SMILES', 'MOL', 'MOL2')".format(format))
 
@@ -49,9 +50,55 @@ def generateConformer(format, value, needAddHs):
 
     return mol.GetConformer(conformerID)
 
+def uniform_random_rotateMatrix():
+    # http://bluewidz.blogspot.com/2017/09/blog-post_30.html
+    # np.dot(v, R)で変換
+    x0 = np.random.random()
+    y1 = 2*math.pi*np.random.random()
+    y2 = 2*math.pi*np.random.random()
+    r1 = math.sqrt(1.0-x0)
+    r2 = math.sqrt(x0)
+    u0 = math.cos(y2)*r2
+    u1 = math.sin(y1)*r1
+    u2 = math.cos(y1)*r1
+    u3 = math.sin(y2)*r2
+    coefi = 2.0*u0*u0-1.0
+    coefuu = 2.0
+    coefe = 2.0*u0
+    R = np.zeros(shape=(3, 3))
+    R[0, 0] = coefi+coefuu*u1*u1
+    R[1, 1] = coefi+coefuu*u2*u2
+    R[2, 2] = coefi+coefuu*u3*u3
+
+    R[1, 2] = coefuu*u2*u3-coefe*u1
+    R[2, 0] = coefuu*u3*u1-coefe*u2
+    R[0, 1] = coefuu*u1*u2-coefe*u3
+
+    R[2, 1] = coefuu*u3*u2+coefe*u1
+    R[0, 2] = coefuu*u1*u3+coefe*u2
+    R[1, 0] = coefuu*u2*u1+coefe*u3
+
+    return R
 
 # 溶液構造を生成
 def mksolv(solventconf, soluteconf, solventNum, soluteNum):
+    #np.random.seed(seed)
+    #
+    #分子内の最大距離を算出(溶媒溶質で大きい方を採用)
+    #    そのサイズの立方体を並べてそこに回転させた分子を配置していく
+    #
+    #solventNum+soluteNumの中からsoluteNumの数だけランダムに数を取り出す
+    #    その番に溶質を配置する
+    #
+    #atomPositionList = conf.GetPositions()
+    #solventNum+soluteNum回ループ
+    #    R = uniform_random_rotateMatrix()
+    #    rotated = np.dot(atomPositionList, R)
+    #    x,y,z >= 0の境界面にrotatedをくっつけるように平行移動
+    #
+    #
+    #
+
     return
 
 # ファイルに保存
@@ -79,6 +126,8 @@ if __name__ == '__main__':
 
     if args.solute_format is None and args.solute is not None:
         parser.error("The '--solute' argument requires the '--solute-format'")
+    if args.solute is not None and args.solute_num == 0:
+        parser.error("The '--solute_num' argument must be greater than 0")
 
     # バージョン確認
     print('python:{}'.format(platform.python_version()))
